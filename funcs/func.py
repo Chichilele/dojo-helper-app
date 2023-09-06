22f242232610cf5fa4cf20101885f5a833f3dfca
G <g.castro@degould.com>
2023-09-07 00:00:59 +0100

    clean up

--- funcs/func.py
+++ funcs/func.py
@@ -1,5 +1,2 @@
 def sum(x: float, y: float) -> float:
     return x + y
-
-def sub(x: float, y: float) -> float:
-    return x - y
--- tests/test_func.py
+++ tests/test_func.py
@@ -6,8 +6,5 @@ class Test(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(func.sum(2,3), 5)
 
-    def test_sub(self):
-        self.assertEqual(func.sub(3,2), 1)
-
 if __name__ == "__main__":
     unittest.main()
